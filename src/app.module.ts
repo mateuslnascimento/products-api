42f895fae9c480de3a9667fc7ced1325df2ce100
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-10-10 00:29:34 -0300

    Setting up Environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTIONSTRING),
     ProductsModule,
   ],
 })
