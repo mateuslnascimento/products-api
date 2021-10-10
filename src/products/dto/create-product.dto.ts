f8cc9898472a0cacde650dfc1d552f15748ac767
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-10-10 00:47:31 -0300

    Inserting payload validations due request

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   category: string;
+
+  @IsNotEmpty()
   description: string;
 }
